bddb4f561fc7fa1c07826d9001d9bb3956df0567
DeepKolos <k1206521365k@126.com>
2017-11-15 00:01:04 +0800

    update readme

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <div class="title">
+      vue当中行为比较规范的popup~<br>
+      同时支持很丰富的过度动画~<br>
+      强大的位置定位~<br><br>
+    </div>
     <a class="cell" :href="'#'+nav.path" v-for="(nav, key) in navs" :key="key">{{ nav.name }}</a>
   </div>
 </template>
@@ -24,3 +29,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .title{
+    font-size: 19px;
+  }
+</style>
