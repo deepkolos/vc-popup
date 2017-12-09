be25d526b9f2aa7412ab9a5fdffe5aa6b4d4bd4e
DeepKolos <k1206521365k@126.com>
2017-12-10 00:08:17 +0800

    fix

--- example/main.js
+++ example/main.js
@@ -2,8 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-// import PopUp from '../src'
-import PopUp from '../lib/vc-popup'
+import PopUp from '../src'
+// import PopUp from '../lib/vc-popup'
 import router from './router'
 import './assets/animate.min.css'
 import './assets/animated-preset.css'
@@ -11,9 +11,7 @@ import './assets/demo.css'
 
 Vue.config.productionTip = false
 
-console.log(PopUp)
 Vue.use(PopUp)
-console.log(Vue.prototype)
 
 /* eslint-disable no-new */
 new Vue({
