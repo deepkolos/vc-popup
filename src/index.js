25e8454e3deb6839bf0d06c432eab179e7fe8045
DeepKolos <k1206521365k@126.com>
2017-12-10 12:34:37 +0800

    发布测试

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import PopupCalendar from './components/popup-calendar/index'
 import PopupDatetimePicker from './components/popup-datetime-picker/index'
 import popupRegister from './components/popup-base/popup-register'
 
-const version = '1.0.5'
+const version = '1.0.7'
 const install = function (Vue, config = {}) {
   if (install.installed) return
   Vue.component(GestureTilePress.name, GestureTilePress)
