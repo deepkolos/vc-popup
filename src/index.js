8345a0c6c0792bd7575d7267190c6597996dcdcc
DeepKolos <k1206521365k@126.com>
2018-01-25 13:33:58 +0800

    version 1.0.20

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import PopupPicker from '../packages/popup-picker/install'
 import PopupCalendar from '../packages/popup-calendar/install'
 import PopupDatetimePicker from '../packages/popup-datetime-picker/install'
 
-const version = '1.0.18'
+const version = '1.0.20'
 const install = function (Vue, config = {}) {
   if (install.installed) return
   Vue.use(PopupBottomMenu)
