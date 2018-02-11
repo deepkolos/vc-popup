9657aab2b3dbd37fa00141b3b2d342331878698e
DeepKolos <k1206521365k@126.com>
2018-02-11 21:19:37 +0800

    添加tile-press的npm包

---
+++ packages/popup-effect-tile-press/install.js
@@ -0,0 +1,17 @@
+
+const version = '0.1.5'
+const install = function (Vue, config = {}) {
+  if (install.installed) return
+  Vue.use(require('vc-popup-base'))
+  require('../../src/components/popup-effect-tile-press')
+}
+
+// auto install
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default {
+  install,
+  version
+}
